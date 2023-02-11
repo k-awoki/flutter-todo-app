76ff5bfd5a859723558320bb6a764749cf69dbeb
k-awoki <aoki.konosuke@gmail.com>
2023-02-12 00:16:33 +0900

    TextFieldとListを追加

--- lib/features/list/list.dart
+++ lib/features/list/list.dart
@@ -8,34 +8,54 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  void _incrementCounter() {
-    setState(() {});
+  String _text = '';
+  final List<String> _tasks = [];
+
+  void _handleText(String text) {
+    _text = text;
+  }
+
+  void _handleButtonTapped() {
+    setState(() {
+      _tasks.add(_text);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Todo'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many:',
+          children: [
+            Row(
+              children: [
+                Flexible(child: TextField(onChanged: _handleText)),
+                const SizedBox(width: 8),
+                TextButton(
+                  onPressed: _handleButtonTapped,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white),
+                  child: const Text('登録'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Flexible(
+              child: ListView.builder(
+                itemCount: _tasks.length,
+                itemBuilder: ((context, index) {
+                  return Text(_tasks[index]);
+                }),
+              ),
             ),
+            const Spacer(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
