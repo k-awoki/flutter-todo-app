09b162621c8a08df5d5999938543a614f52b06d4
k-awoki <aoki.konosuke@gmail.com>
2023-02-12 10:07:30 +0900

    登録ボタン追加

--- lib/features/register/register_page.dart
+++ lib/features/register/register_page.dart
@@ -3,14 +3,23 @@ import 'package:flutter/material.dart';
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
+  void _tappedRegisterButton() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Registor')),
       body: Column(
-        children: const [
-          TextField(),
-          Spacer(),
+        children: [
+          const TextField(),
+          const SizedBox(height: 8),
+          TextButton(
+            onPressed: _tappedRegisterButton,
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.blue, foregroundColor: Colors.white),
+            child: const Text('登録'),
+          ),
+          const Spacer(),
         ],
       ),
     );
